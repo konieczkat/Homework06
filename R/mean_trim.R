c896351678a3211385dbab89c2a2f0fe50270998
konieczkat <konieczkat@outlook.com>
2023-09-25 11:17:55 -0400

    Update mean_trim.R

--- R/mean_trim.R
+++ R/mean_trim.R
@@ -1,7 +1,10 @@
 mean_trim <- function(x){
-    sorted_list<- sort(x)
-    small <- min(sorted_list)
-    large <- max(sorted_list)
-    new_list <- sorted_list[-c(small, large)]
-    return(mean(new_list))
+  if(!(is.numeric(x))){
+    stop("Vector must be numeric")
   }
+  sorted_list<- sort(x)
+  small <- min(sorted_list)
+  large <- max(sorted_list)
+  new_list <- sorted_list[-c(small, large)]
+  return(mean(new_list))
+}
